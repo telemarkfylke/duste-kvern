a924d0cad4f5d5c4af04f70040f6a977eb7822a9
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-02-06 10:00:48 +0100

    config

--- config.js
+++ config.js
@@ -12,6 +12,23 @@ module.exports = {
   APPREG: {
     CLIENT_ID: process.env.APPREG_CLIENT_ID,
     CLIENT_SECRET: process.env.APPREG_CLIENT_SECRET,
-    TENANT_ID: process.env.APPREG_TENANT_ID
-  }
+    TENANT_ID: process.env.APPREG_TENANT_ID,
+    TENANT_NAME: process.env.APPREG_TENANT_NAME
+  },
+  APPREG_VTFK: {
+    CLIENT_ID: process.env.APPREG_VTFK_CLIENT_ID,
+    CLIENT_SECRET: process.env.APPREG_VTFK_CLIENT_SECRET,
+    TENANT_ID: process.env.APPREG_VTFK_TENANT_ID,
+    TENANT_NAME: process.env.APPREG_VTFK_TENANT_NAME
+  },
+  GRAPH: {
+    SCOPE: process.env.GRAPH_SCOPE || 'https://graph.microsoft.com/.default',
+    URL: process.env.GRAPH_URL || 'https://graph.microsoft.com'
+  },
+  FINTFOLK: {
+    SCOPE: process.env.FINTFOLK_SCOPE,
+    URL: process.env.FINTFOLK_URL
+  },
+  PS1_SCRIPTS_PATH: process.env.PS1_SCRIPTS_PATH || 'D:/blabalabal',
+  MAX_BUFFER: process.env.MAX_BUFFER || 1024 * 10000
 }
