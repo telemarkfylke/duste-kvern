034a4db404c6aafcddb79d25e3387301d79e6326
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-02-09 13:12:01 +0100

    dev tools

--- dev-tools/insert-test-reports.js
+++ dev-tools/insert-test-reports.js
@@ -1,11 +1,12 @@
 (async () => {
   const { getMongoClient, closeMongoClient } = require('../lib/mongo-client')
   const { MONGODB } = require('../config')
-  const testReports = require('./data/test-reports')
+  const generateTestReports = require('./data/test-reports')
   const client = await getMongoClient()
 
   const db = client.db(MONGODB.DB_NAME)
   const collection = db.collection(MONGODB.REPORT_COLLECTION)
+  const testReports = generateTestReports(5)
   try {
     await collection.insertMany(testReports)
   } catch (error) {
