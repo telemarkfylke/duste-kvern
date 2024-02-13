fd3ee2cf4b55d185875626159789ccccc204a584
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-02-13 12:21:47 +0100

    more audit logs

--- systems/azure/get-data.js
+++ systems/azure/get-data.js
@@ -64,13 +64,15 @@ const getData = async (user) => {
   const graphUserAuthMethods = graphUserAuth?.value && graphUserAuth.value.length && graphUserAuth.value.filter(method => !method['@odata.type'].includes('passwordAuthenticationMethod'))
 
   logger('info', ['azure-get-data', 'fetching signins for user'])
-  const graphUserSignIns = await callGraph(`v1.0/auditLogs/signIns?$filter=userPrincipalName eq '${user.userPrincipalName}' and createdDateTime gt ${entraIdDate()}&$top=100`, accessToken)
+  const today = new Date()
+  const thirtyDaysBack = new Date(new Date().setDate(today.getDate() - 30))
+  const graphUserSignIns = await callGraph(`v1.0/auditLogs/signIns?$filter=userPrincipalName eq '${user.userPrincipalName}' and createdDateTime gt ${entraIdDate(thirtyDaysBack)}&$top=100`, accessToken)
 
-  const userSignInErrors = graphUserSignIns.value.filter(signIn => signIn.status.errorCode === 50126)
-  const userSignInSuccess = graphUserSignIns.value.filter(signIn => signIn.status.errorCode === 0) // LEgg til sort om den en eller annen gang ikke kommer sortert på dato
+  const userSignInErrors = graphUserSignIns.value.filter(signIn => { return (entraIdDate(new Date(signIn.createdDateTime) === entraIdDate(today)) && signIn.status.errorCode === 50126) }) // Only signinErrors from today
+  const userSignInSuccess = graphUserSignIns.value.filter(signIn => signIn.status.errorCode === 0) // Legg til sort om den en eller annen gang ikke kommer sortert på dato
 
   logger('info', ['azure-get-data', 'fetching riskyuser data for user'])
-  const graphRiskyUser = await callGraph(`v1.0//identityProtection/riskyUsers?$filter=userPrincipalName eq '${user.userPrincipalName}' and riskState ne 'dismissed'`, accessToken)
+  const graphRiskyUser = await callGraph(`v1.0/identityProtection/riskyUsers?$filter=userPrincipalName eq '${user.userPrincipalName}' and riskState ne 'dismissed'`, accessToken)
 
   return {
     ...userData,
