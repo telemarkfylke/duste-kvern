ff82aba204df5571c33361adb2c4b7a6cbe9d098
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-02-20 08:24:42 +0100

    bug

--- user-types/otVTFK.js
+++ user-types/otVTFK.js
@@ -27,7 +27,7 @@ const systemsAndTests = [
           const data = {
             enabled: systemData.enabled
           }
-          if (!data.enabled) return error({ message: 'Konto er ikke aktivert 😬', raw: data, solution: 'Bruker må aktivere sin konto via minkonto.vtfk.no eller servicedesk kan gjøre det direkte i ${systemNames.ad}`' })
+          if (!data.enabled) return error({ message: 'Konto er ikke aktivert 😬', raw: data, solution: `Bruker må aktivere sin konto via minkonto.vtfk.no eller servicedesk kan gjøre det direkte i ${systemNames.ad}` })
           return success({ message: 'Kontoen er aktivert', raw: data })
         }
       },
